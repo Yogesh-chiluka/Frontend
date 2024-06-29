3fc5ab211b6f9714531c2622f4212f78b01900e9
Yogesh Chiluka <72222615+Yogesh-chiluka@users.noreply.github.com>
2024-06-29 18:40:32 +0000

    Add Layout and components - install tailwindcss - add postcss.config.js - Layout - EmptyVideoPage component - VideoListingPageCards - VideoListingPageList

---
+++ src/components/EmptyVideoPage.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function EmptyVideoPage(){
+
+    return(<>
+    <section class="w-full pb-[70px] sm:ml-[70px] sm:pb-0 lg:ml-0"><div class="flex h-full items-center justify-center"><div class="w-full max-w-sm text-center"><p class="mb-3 w-full"><span class="inline-flex rounded-full bg-[#E4D3FF] p-2 text-[#AE7AFF]"></span></p><h5 class="mb-2 font-semibold">No videos available</h5><p>There are no videos here available. Please try to search some thing else.</p></div></div></section>
+        
+    </>)
+}
