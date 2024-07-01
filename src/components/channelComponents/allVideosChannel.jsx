e238e3986bd96e10fa29f0f0600c4bd61154876d
Yogesh Chiluka <72222615+Yogesh-chiluka@users.noreply.github.com>
2024-07-01 15:52:46 +0000

    Add layouts and components for Channel Page

---
+++ src/components/channelComponents/allVideosChannel.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function allVideosChannel(){
+
+    return(
+        <div class="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4 pt-2"><div class="w-full"><div class="relative mb-2 w-full pt-[56%]"><div class="absolute inset-0"><img src="https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1" alt="JavaScript Fundamentals: Variables and Data Types" class="h-full w-full"></div><span class="absolute bottom-1 right-1 inline-block rounded bg-black px-1.5 text-sm">20:45</span></div>
+<h6 class="mb-1 font-semibold">JavaScript Fundamentals: Variables and Data Types</h6>
+<p class="flex text-sm text-gray-200">10.3k&nbsp;Views · 44 minutes ago</p>
+</div>
+</div>
+
+    )
+}
---
+++ src/components/video.jsx
@@ -0,0 +1,9 @@
+
+
+
+export default function Video(){
+    return(
+        <div class="relative mb-4 w-full pt-[56%]"><div class="absolute inset-0"><video class="h-full w-full" controls="" autoplay="" muted=""><source src="https://res.cloudinary.com/dfw5nnic5/video/upload/v1695117968/Sample_1280x720_mp4_b4db0s.mp4" type="video/mp4"></video></div></div>
+    
+    )
+}
