1cd6c6debc84aac9306a03f7d8d9a9ee114874c7
Yogesh Chiluka <72222615+Yogesh-chiluka@users.noreply.github.com>
2024-07-02 17:55:28 +0000

    Add Routes to App.jsx and Install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,42 @@ import Layout from './components/Layout'
 
 import './App.css'
 
+import {
+  RouterProvider,
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route, 
+  Navigate
+} from "react-router-dom";
+
+import EmptyVideoPage from './components/EmptyVideoPage'
+import VideoListingPageCards from './components/VideoListingPageCards'
+import VideoListingPageList from './components/VideoListingPageList'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Layout />}
+    >
+      <Route path='/Home' element={<EmptyVideoPage />} >
+        <Route path='/Home/Empty' element={<EmptyVideoPage />} />
+        <Route path='/Home/VideoCards' element={<VideoListingPageCards />} />
+        <Route path='/Home/VideoList' element={<VideoListingPageList />} />
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <Layout/>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
 export default App
+//<Route index element={<Index /> } />
+
+//<Route path='/EmptyVideoPage' element={<EmptyVideoPage />} loader={ReposLoader} errorElement={<ErrorPage/>} />
